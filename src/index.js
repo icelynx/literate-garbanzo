a68aff18fb7eed600a5711e150dc3e93e7166faa
Aki Ryynänen <aki.p.ryynanen@gmail.com>
2017-11-21 19:25:19 +0200

    Add developer tools to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('Loaded')
+import React from 'react'
+import {render} from 'react-dom'
+import {AppContainer} from 'react-hot-loader'
+import App from './app'
+
+const wrapper = document.querySelector('#untitled')
+
+render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  wrapper
+)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const HotAppLoader = require('./app').default
+    render(
+      <AppContainer>
+        <HotAppLoader />
+      </AppContainer>,
+      wrapper
+    )
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -7,7 +8,34 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    publicPath: '/',
+    compress: true,
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: 'eslint-loader',
+        exclude: [
+          /node_modules/,
+          /dist/
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
     new HtmlWebpackPlugin({
       title: 'Untitled',
       template: path.join(__dirname, 'src', 'index.ejs')
