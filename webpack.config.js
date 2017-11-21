5c4b109ff446053eb96b5b8394997cbd00ec47ee
Aki Ryynänen <aki.p.ryynanen@gmail.com>
2017-11-21 19:45:04 +0200

    Fix hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,12 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
